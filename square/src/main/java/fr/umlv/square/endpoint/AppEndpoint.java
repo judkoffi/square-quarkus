ab82312f1a06f72ea799fbbbf3a88ecc0d774849
judi <judkoffi@gmail.com>
2019-10-22 15:52:44 +0200

    begin deploy response class

--- square/src/main/java/fr/umlv/square/endpoint/AppEndpoint.java
+++ square/src/main/java/fr/umlv/square/endpoint/AppEndpoint.java
@@ -1,16 +1,16 @@
 package fr.umlv.square.endpoint;
 
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
+import java.util.Random;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 
 import fr.umlv.square.model.request.RequestDeploy;
+import fr.umlv.square.model.response.ResponseDeploy;
 
 @Path("/app")
 public class AppEndpoint {
@@ -20,15 +20,9 @@ public class AppEndpoint {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response deploy(RequestDeploy request) {
-		
-        /*Jsonb jsonb = JsonbBuilder.create();
-        
-        RequestDeploy p = jsonb.fromJson(request, RequestDeploy.class);
-        
-		System.out.println("request " + p);*/
-		
-		System.out.println(request);
-
-		return Response.ok().entity("hello toto").build();
+		ResponseDeploy response = new ResponseDeploy();
+		response.setId(new Random().nextInt());
+		response.setApp(request.getAppName());
+		return Response.ok().entity(response).build();
 	}
 }
--- square/src/main/java/fr/umlv/square/model/response/ResponseDeploy.java
+++ square/src/main/java/fr/umlv/square/model/response/ResponseDeploy.java
@@ -1,7 +1,68 @@
 package fr.umlv.square.model.response;
 
 public class ResponseDeploy {
-	
-	
+	private int id;
+	private String app;
+	private String port;
+	private String servicePort;
+	private String dockerInstance;
+
+	public ResponseDeploy() {
+
+	}
+
+	public ResponseDeploy(int id, String app, String port, String servicePort, String dockerInstance) {
+		super();
+		this.id = id;
+		this.app = app;
+		this.port = port;
+		this.servicePort = servicePort;
+		this.dockerInstance = dockerInstance;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getApp() {
+		return app;
+	}
+
+	public void setApp(String app) {
+		this.app = app;
+	}
+
+	public String getPort() {
+		return port;
+	}
+
+	public void setPort(String port) {
+		this.port = port;
+	}
+
+	public String getServicePort() {
+		return servicePort;
+	}
+
+	public void setServicePort(String servicePort) {
+		this.servicePort = servicePort;
+	}
+
+	public String getDockerInstance() {
+		return dockerInstance;
+	}
+
+	public void setDockerInstance(String dockerInstance) {
+		this.dockerInstance = dockerInstance;
+	}
+
+	@Override
+	public String toString() {
+		return "{\"id\":" + id + ", \"app\": " + app + "}";
+	}
 
 }
