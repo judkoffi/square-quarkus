5ef4ea6a625ed7d446847a18f4b184a446e38f51
MelissaDaCosta <mel-dacosta@hotmail.fr>
2019-10-22 13:29:53 +0200

    add square

---
+++ square/src/test/java/fr/umlv/square/NativeExampleIT.java
@@ -0,0 +1,9 @@
+package fr.umlv.square;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeExampleIT extends ExampleTest {
+
+    // Execute the same tests but in native mode.
+}
