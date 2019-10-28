0146b55d3fe4772cc4bf96ddf1a78e69d2cb66b3
judi <judkoffi@gmail.com>
2019-10-29 00:36:12 +0100

    add methode use to strat docker with app/deploy endpoint

--- square/src/main/java/fr/umlv/square/service/DockerService.java
+++ square/src/main/java/fr/umlv/square/service/DockerService.java
@@ -3,8 +3,8 @@ package fr.umlv.square.service;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Path;
 
 import javax.enterprise.context.ApplicationScoped;
 
@@ -15,9 +15,11 @@ public class DockerService {
 
 	private final static String DOCKER_IMAGE_DIRECTORY = "docker-images/";
 
+	private final static String APPS_DIRECTORY = "apps/";
+
 	static {
 		DOCKERFILE_TEMPLATE = "FROM hirokimatsumoto/alpine-openjdk-11\n" + "WORKDIR /app\n"
-				+ "COPY {{1}} /app/app.jar\n" + "RUN chmod 775 /app\n" + "EXPOSE {{2}}\n" + "CMD java -jar app.jar";
+				+ "COPY {{1}}.jar /app/app.jar\n" + "RUN chmod 775 /app\n" + "EXPOSE {{2}}\n" + "CMD java -jar app.jar";
 	}
 
 	private final ProcessBuilder processBuilder;
@@ -26,34 +28,56 @@ public class DockerService {
 		this.processBuilder = new ProcessBuilder();
 	}
 
+	private String getOutput(InputStream outpuStream) throws IOException {
+		var reader = new BufferedReader(new InputStreamReader(outpuStream));
+		var builder = new StringBuilder();
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			builder.append(line);
+			builder.append(System.getProperty("line.separator"));
+		}
+
+		return builder.toString();
+
+	}
+
 	public String buildImage(String appName, int port) {
 
+		var imageFile = DOCKERFILE_TEMPLATE.replace("{{1}}", appName).replace("{{2}}", "" + port);
+
+		var imagePath = DOCKER_IMAGE_DIRECTORY + "Dockerfile." + appName;
+
+		var createDockerfileCommand = "echo \"" + imageFile + "\" > " + imagePath;
+
+		processBuilder.directory(new File("../"));
+
+		// processBuilder.command("/bin/bash", "-c", "pwd");
+
+		var builImageCommand = "docker build -f " + imagePath + " -t " + appName + " " + APPS_DIRECTORY;
+
+		var listImagesCommand = "docker images";
+
+		var runDockerCommand = "docker run -p " + port + ":8080 " + appName;
+
+		System.out.println(runDockerCommand);
+
+		var dockerPsCommand = "docker ps";
+
 		try {
-			var imageFile = DOCKERFILE_TEMPLATE.replace("{{1}}", appName).replace("{{2}}", "" + port);
 
-			var createDockerfileCommand = "echo \"" + imageFile + "\" > " + DOCKER_IMAGE_DIRECTORY + "Dockerfile."
-					+ appName;
 
-			processBuilder.directory(new File("../"));
+			///processBuilder.command("/bin/bash", "-c", createDockerfileCommand).start().getInputStream();
 
-			// processBuilder.command("/bin/bash", "-c", "pwd");
+			var outputStream = processBuilder.command("/bin/bash", "-c", builImageCommand).start().getInputStream();
 
-			processBuilder.command("/bin/bash", "-c", createDockerfileCommand);
+			outputStream = processBuilder.command("/bin/bash", "-c", runDockerCommand).start().getInputStream();
 
-			var prefix = "docker build -f docker-images/Dockerfile." + appName + "-t helloapp apps/";
+			outputStream = processBuilder.command("/bin/bash", "-c", dockerPsCommand).start().getInputStream();
 
-			var reader = new BufferedReader(new InputStreamReader(processBuilder.start().getInputStream()));
-			var builder = new StringBuilder();
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				builder.append(line);
-				builder.append(System.getProperty("line.separator"));
-			}
-
-			return builder.toString();
-
-		} catch (IOException exception) {
-			return exception.getMessage();
+			return getOutput(outputStream);
+		} catch (IOException e) {
+			return e.getMessage();
 		}
 	}
+
 }
