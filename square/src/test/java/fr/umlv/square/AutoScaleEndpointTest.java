b256bc13275dfd17d7507d9336ab48ff67cdf271
MelissaDaCosta <mel-dacosta@hotmail.fr>
2019-10-30 21:41:53 +0100

    add AutoScaleEndpointTest

--- square/src/test/java/fr/umlv/square/AutoScaleEndpointTest.java
+++ square/src/test/java/fr/umlv/square/AutoScaleEndpointTest.java
@@ -1,5 +1,13 @@
 package fr.umlv.square;
 
 public class AutoScaleEndpointTest {
+	
+	@Test
+	public void testAutoScaleUpdateEndpoint() {
+		var expected = "{\"todomvc:8082\": \"need to start 1 instance(s)\", \"demo:8083\": \"need to stop 2 instance(s)\"}";
 
+		given().contentType(ContentType.JSON).body("{\"todomvc:8082\": 1, \"demo:8083\" : 2 }").when().post("/auto-scale/update").then()
+				.statusCode(200).assertThat().body(is(expected));
+
+	}
 }
