a33bb18b7907469e9aa74898d7bf3e567db896a9
MelissaDaCosta <mel-dacosta@hotmail.fr>
2019-11-05 17:57:47 +0100

    add librairie

--- square-client/src/fr/umlv/square/client/Main.java
+++ square-client/src/fr/umlv/square/client/Main.java
@@ -3,8 +3,18 @@ package fr.umlv.square.client;
 import java.io.IOException;
 
 public class Main {
-  public static void main(String[] args) throws IOException, InterruptedException {
-    var squareClient = new SquareClient();
-    squareClient.sendLog();
-  }
+	private static final String PROPERTIES_PATH = "client.properties";
+	public static void main(String[] args) {
+
+		try {
+			var config = ClientConfig.fromFile(PROPERTIES_PATH);
+			var squareClient = new SquareClient(config);
+			squareClient.sendLog();
+		} catch (IOException e) {
+			System.exit(-1);
+		} catch (InterruptedException e) {
+			// TODO
+		}
+
+	}
 }
--- square-client/src/fr/umlv/square/client/SquareClient.java
+++ square-client/src/fr/umlv/square/client/SquareClient.java
@@ -5,31 +5,50 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.HashMap;
+import java.util.StringJoiner;
 
 public class SquareClient {
-  private final String squareHost = "http://192.168.43.184";
-  private final int squarePort = 5050;
 
-  public SquareClient() {
+	private final String squarUrl;
 
-  }
+	public SquareClient(ClientConfig clientConfig) {
+		this.squarUrl = "http://" + clientConfig.squareHost + ":" + clientConfig.squarePort;
+	}
 
-  public void sendLog() throws IOException, InterruptedException {
-    var uri = URI.create(squareHost + ":" + squarePort + "/kawai/send-log");
+	public void sendLog() throws IOException, InterruptedException {
+		var uri = squarUrl + "/kawai/send-log";
+		while (true) {
+			new Thread(() -> {
+				var i = 0;
+				var map = new HashMap<String, String>();
+				var body = "COUCOU " + i;
+				map.put("message", body);
+				var client = HttpClient.newHttpClient();
 
-    var body = "{\"HoneyBee\": \"COUCOU\"}";
+				var builder = new StringJoiner(",", "{", "}");
 
-    var client = HttpClient.newHttpClient();
-    var request = HttpRequest
-      .newBuilder()
-      .uri(uri)
-      .header("Content-Type", "application/json")
-      .POST(HttpRequest.BodyPublishers.ofString(body))
-      .build();
+				map.entrySet().forEach((elt) -> builder.add("\"" + elt.getKey() + "\":\"" + elt.getValue() + "\""));
 
-    var response = client.send(request, BodyHandlers.ofString());
-    System.out.println(response.statusCode());
-    System.out.println(response.body());
-  }
+				System.out.println(builder.toString());
+				var request = HttpRequest.newBuilder().uri(URI.create(uri)).header("Content-Type", "application/json")
+						.POST(HttpRequest.BodyPublishers.ofString(builder.toString())).build();
+				i++;
 
+				try {
+					var response = client.send(request, BodyHandlers.ofString());
+					System.out.println(response.statusCode());
+					System.out.println(response.body());
+				} catch (IOException | InterruptedException e1) {
+					throw new AssertionError();
+				}
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					throw new AssertionError();
+
+				}
+			}).start();
+		}
+	}
 }
