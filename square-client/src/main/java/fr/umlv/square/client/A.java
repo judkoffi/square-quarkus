e0615a3ecb700d13ec73ed5f422b76e3049e400f
judi <judkoffi@gmail.com>
2019-11-08 12:44:38 +0100

    factorise client job code

---
+++ square-client/src/main/java/fr/umlv/square/client/A.java
@@ -0,0 +1,53 @@
+package fr.umlv.square.client;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+
+// TODO: Find more significant name
+public class A {
+  private final static String OUTPUT_FILE = "outputLogfile.log";
+  private final static String ERREUR_FILE = "errorLogfile.log";
+  private final SquareClient squareClient;
+
+  private int outputReadingIndex;
+  private int erreurReadingIndex;
+
+  public A() {
+    this.outputReadingIndex = 0;
+    this.erreurReadingIndex = 0;
+    this.squareClient = new SquareClient(ClientConfig.fromEnv());
+  }
+
+  public boolean startApp() {
+    var runApp = "java -jar app.jar";
+    var processBuilder = new ProcessBuilder().inheritIO(); // launch processBuilder in same console
+                                                           // that runApp command
+    var outputLogFile = new File(OUTPUT_FILE);
+    var erreurLogFile = new File(ERREUR_FILE);
+    processBuilder.redirectOutput(outputLogFile);
+    processBuilder.redirectError(erreurLogFile);
+
+    try {
+      var process = processBuilder.command("sh", "-c", runApp).start();
+      return process.isAlive();
+    } catch (IOException e1) {
+      return false;
+    }
+  }
+
+  public void doWork() throws IOException {
+    var list = Files.lines(Path.of(OUTPUT_FILE)).collect(Collectors.toList());
+    var newMessageList = list.subList(outputReadingIndex, list.size());
+    var msg = newMessageList.toString();
+    outputReadingIndex = list.size();
+    new Thread(() -> squareClient.sendLog(msg, LogType.INFO)).start();
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException e) {
+      throw new AssertionError();
+    }
+  }
+}
--- square-client/src/main/java/fr/umlv/square/client/Main.java
+++ square-client/src/main/java/fr/umlv/square/client/Main.java
@@ -1,54 +1,23 @@
 package fr.umlv.square.client;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.stream.Collectors;
 
 public class Main {
-  private static int readIndex;
-
-  private final static String OUTPUT_FILE = "outputLogfile.log";
-  private final static String ERREUR_FILE = "errorLogfile.log";
-
-
   public static void main(String[] args) {
-    var config = ClientConfig.fromEnv();
-    var squareClient = new SquareClient(config);
-
-    var runApp = "java -jar app.jar";
-    var processBuilder = new ProcessBuilder().inheritIO(); // launch processBuilder in same console
-                                                           // that runApp command
-    var outputLogFile = new File(OUTPUT_FILE);
-    var erreurLogFile = new File(ERREUR_FILE);
-    processBuilder.redirectOutput(outputLogFile);
-    processBuilder.redirectError(erreurLogFile);
-
-    Process process = null;
-    try {
-      process = processBuilder.command("sh", "-c", runApp).start();
-    } catch (IOException e1) {
-      e1.printStackTrace();
+    var executor = new A();
+    if (!executor.startApp()) {
+      System.err.println("App not start");
+      System.exit(-1);
     }
 
     var i = 0;
     while (i < 50) {
       try {
-        var list = Files.lines(Path.of(OUTPUT_FILE)).collect(Collectors.toList());
-        var newMessageList = list.subList(readIndex, list.size());
-        var msg = newMessageList.toString();
-        readIndex = list.size();
-        new Thread(() -> squareClient.sendLog(msg, LogType.INFO)).start();
-        i++;
-        try {
-          Thread.sleep(5000);
-        } catch (InterruptedException e) {
-          throw new AssertionError();
-        }
-      } catch (IOException e1) {
-        e1.printStackTrace();
+        executor.doWork();
+      } catch (IOException e) {
+        e.printStackTrace();
       }
+      i++;
     }
   }
 }
