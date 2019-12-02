8e868c8ffa5b8326531a72a5f53923ffbe304e6b
judi <judkoffi@gmail.com>
2019-12-02 10:06:07 +0100

    update test

--- square/src/test/java/fr/umlv/square/LogEndpointTest.java
+++ square/src/test/java/fr/umlv/square/LogEndpointTest.java
@@ -3,7 +3,6 @@ package fr.umlv.square;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.IOException;
 import java.net.URI;
@@ -49,13 +48,13 @@ public class LogEndpointTest {
   }
 
   @Test
-  @Order(0)
+  @Order(1)
   public void clear() {
     clean();
   }
 
   @Test
-  @Order(1)
+  @Order(2)
   public void testGoodRequestTimelogsWithoutDeployApp() {
     given()
       .contentType(ContentType.JSON)
@@ -68,7 +67,7 @@ public class LogEndpointTest {
   }
 
   @Test
-  @Order(2)
+  @Order(3)
   public void testBagRequestTimelogs() {
     given().contentType(ContentType.JSON).when().get("/logs/a").then().statusCode(400).assertThat();
     given()
@@ -93,7 +92,7 @@ public class LogEndpointTest {
   }
 
   @Test
-  @Order(3)
+  @Order(4)
   public void testGoodRequestTimelogs() throws InterruptedException {
     TimeUnit.SECONDS.sleep(20); // to be sure that logs are stored in DB
     var result = given()
@@ -110,7 +109,7 @@ public class LogEndpointTest {
   }
 
   @Test
-  @Order(4)
+  @Order(5)
   public void testGoodRequestTimelogsWIthAppNameFilter() {
     var result = given()
       .contentType(ContentType.JSON)
@@ -125,93 +124,9 @@ public class LogEndpointTest {
     assertTrue(result.size() >= 3);
   }
 
-  @Test
-  @Order(5)
-  public void testGoodRequestTimelogsWIthInstanceFilter() throws InterruptedException {
-    var list = given()
-      .contentType(ContentType.JSON)
-      .when()
-      .get("/app/list")
-      .then()
-      .statusCode(200)
-      .extract()
-      .as(ModelClassesForTests.getDeployResponseTypeRef());
-
-    var tmp = list.stream().filter((p) -> p.appName.equals("fruitapi")).findFirst().get();
-    var tmp2 = list.stream().filter((p) -> p.appName.equals("helloapp")).findFirst().get();
-
-    var resultFruitApi = given()
-      .contentType(ContentType.JSON)
-      .when()
-      .get("/logs/10/" + tmp.dockerInstance)
-      .then()
-      .statusCode(200)
-      .assertThat()
-      .extract()
-      .as(ModelClassesForTests.getLogsResponseTypeRef());
-
-    var resultHelloApp = given()
-      .contentType(ContentType.JSON)
-      .when()
-      .get("/logs/10/" + tmp2.dockerInstance)
-      .then()
-      .statusCode(200)
-      .assertThat()
-      .extract()
-      .as(ModelClassesForTests.getLogsResponseTypeRef());
-
-    assertAll(() ->
-    {
-      assertTrue(resultFruitApi.size() >= 3);
-      assertTrue(resultHelloApp.size() >= 3);
-    });
-  }
 
   @Test
   @Order(6)
-  public void testGoodRequestTimelogsWIthIdFilter() throws InterruptedException {
-    var list = given()
-      .contentType(ContentType.JSON)
-      .when()
-      .get("/app/list")
-      .then()
-      .statusCode(200)
-      .extract()
-      .as(ModelClassesForTests.getDeployResponseTypeRef());
-
-    var tmp = list.stream().filter((p) -> p.appName.equals("fruitapi")).findFirst().get();
-    var tmp2 = list.stream().filter((p) -> p.appName.equals("helloapp")).findFirst().get();
-
-    var resultFruitApi = given()
-      .contentType(ContentType.JSON)
-      .when()
-      .get("/logs/10/" + tmp.id)
-      .then()
-      .statusCode(200)
-      .assertThat()
-      .extract()
-      .as(ModelClassesForTests.getLogsResponseTypeRef());
-
-    var resultHelloApp = given()
-      .contentType(ContentType.JSON)
-      .when()
-      .get("/logs/10/" + tmp2.id)
-      .then()
-      .statusCode(200)
-      .assertThat()
-      .extract()
-      .as(ModelClassesForTests.getLogsResponseTypeRef());
-
-    assertAll(() ->
-    {
-      assertTrue(resultFruitApi.size() >= 3);
-      assertTrue(resultHelloApp.size() >= 3);
-    });
-  }
-
-
-  @Test
-  @Order(7)
   public void testGoodRequestTimelogs2() throws InterruptedException, IOException {
     var list = given()
       .contentType(ContentType.JSON)
